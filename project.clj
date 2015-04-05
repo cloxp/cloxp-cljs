104c81abafa98e020511cab72f65c34a158e29c4
Robert Krahn <robert.krahn@gmail.com>
2015-04-05 02:47:43 -0700

    0.1.1

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject org.rksm/cloxp-cljs "0.1.1-SNAPSHOT"
+(defproject org.rksm/cloxp-cljs "0.1.1"
   :description "cloxp-cljs"
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-3126"]
-                 [org.rksm/system-files "0.1.3-SNAPSHOT"]
-                 [org.rksm/cloxp-source-reader "0.1.0-SNAPSHOT"]
+                 [org.rksm/system-files "0.1.3"]
+                 [org.rksm/cloxp-source-reader "0.1.1"]
                  [clojurescript-build/clojurescript-build "0.1.5"]
                  [lein-cljsbuild "1.0.5"]
                  [leiningen/leiningen "2.5.1"]]
